0ba65d746a0b5c1727a33901e632e8d7aa693eab
gmmrr <guanming091429@gmail.com>
2024-06-09 16:07:48 +0800

    add log, and allow multi-client

--- client.c
+++ client.c
@@ -1,63 +1,79 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <ctype.h>
-
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #define PORT 9013
 
 int main() {
+
+    printf("Client: Start\n");
+
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[1024] = {0};
 
-    // Creating socket file descriptor
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-
+    // Define addr
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
+    // Creating socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Client: Socket creation error\n");
+        return -1;
+    }
+
     // Convert IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
+        printf("Client: Invalid address/ Address not supported\n");
         return -1;
     }
 
+    // Try connecting to the server
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
+        printf("Client: Connection Failed\n");
         return -1;
     }
+    printf("Client: Connect server [127.0.0.1:%d] success\n", PORT);
 
-    printf("Connect server [127.0.0.1:%d] success\n", PORT);
-
+    // Main client loop
     while (1) {
-        printf("Please input your message: ");
-        fgets(buffer, 1024, stdin);
+        printf("\nPlease input your message: ");
 
-        // Remove the newline character from fgets
+        // Deal with the input
+        fgets(buffer, 1024, stdin);
         size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
+        if (len > 0 && buffer[len - 1] == '\n') { // change newline notation into termination
             buffer[len - 1] = '\0';
         }
 
+        // Send the message to the server
         send(sock, buffer, strlen(buffer), 0);
-        memset(buffer, 0, sizeof(buffer));
-        read(sock, buffer, 1024);
 
-        // Convert received message to uppercase
-        for (int i = 0; buffer[i]; i++) {
-            buffer[i] = toupper(buffer[i]);
+        // Clear the buffer for the next read
+        memset(buffer, 0, sizeof(buffer));
+
+        // Receive the message from the server
+        int valread = read(sock, buffer, 1024);
+
+        if (valread > 0) {
+            printf("Client: Received message from [127.0.0.1:%d]: %s\n", PORT, buffer);
+        } else if (valread == 0) {
+            printf("Client: Server disconnected\n");
+            break;
+        } else {
+            printf("Client: Read error\n");
+            break;
         }
 
-        printf("get receive message from [127.0.0.1:%d]: %s\n", PORT, buffer);
+        // Clear the buffer for the next input
         memset(buffer, 0, sizeof(buffer));
     }
 
+    // Close the client socket
     close(sock);
+    printf("Client: End\n");
     return 0;
 }
--- server.c
+++ server.c
@@ -1,15 +1,17 @@
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include <time.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 9013
+#define LOG_FILE "./serverlog.txt"
 
 void daemonize() {
     pid_t pid;
@@ -24,77 +26,135 @@ void daemonize() {
         exit(EXIT_SUCCESS);
     }
 
-    // Change the file mode mask
-    umask(0);
-
     // Create a new SID for the child process
     if (setsid() < 0) {
         exit(EXIT_FAILURE);
     }
-
-    // Change the current working directory
-    if ((chdir("/")) < 0) {
-        exit(EXIT_FAILURE);
-    }
-
-    // Close out the standard file descriptors
-    // close(STDIN_FILENO);
-    // close(STDOUT_FILENO);
-    // close(STDERR_FILENO);
-
-    // Open logs here
 }
 
-int main() {
-    daemonize();
+void printlog(const char *format, ...) {
+    FILE *logfile;
+    va_list args;
+    time_t now;
+    char timestamp[20];
 
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+    // open the file
+    logfile = fopen(LOG_FILE, "a");
+    if (!logfile) {
+        perror("Log: Open failed");
+        return;
+    }
+
+    // normalise the log message and write in
+    time(&now);
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&now));
+    fprintf(logfile, "%s: ", timestamp);
+    va_start(args, format);
+    vfprintf(logfile, format, args);
+    va_end(args);
+    fprintf(logfile, "\n");
+    fclose(logfile);
+}
+
+void *handle_client(void *arg) {
+    int client_socket = *(int*)arg;
     char buffer[1024] = {0};
+    int valread;
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    printlog("Socket %d: Connected", client_socket);
+
+    while ((valread = read(client_socket, buffer, 1024)) > 0) {
+        // Convert received message to uppercase
+        for (int i = 0; buffer[i]; i++) {
+            buffer[i] = toupper(buffer[i]);
+        }
+
+        // Send the modified message back to the client
+        send(client_socket, buffer, strlen(buffer), 0);
+
+        // Log the received message
+        printlog("Socket %d: Receive message \"%s\"", client_socket, buffer);
+
+        // Clear the buffer for the next read
+        memset(buffer, 0, sizeof(buffer));
     }
 
-    printf("Start server\n");
-
-    // Forcefully attaching socket to the port 9013
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+    if (valread < 0) {
+        perror("read");
     }
 
+    printlog("Socket %d: Disconnected", client_socket);
+    close(client_socket);
+    free(arg);
+
+    return NULL;
+}
+
+
+int main() {
+
+    printlog("Server: Start");
+    daemonize();
+    printlog("Server: Daemonized");
+
+    int server_fd;
+    int client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    int opt = 1; // to ensure that upon server termination and restart, previously used addr can be immediately reused
+    pthread_t thread_id;
+
+    // Define addr
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 9013
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("create socket failed");
+        printlog("Server: Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        printlog("Server: Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
+        printlog("Server: Bind failed");
         exit(EXIT_FAILURE);
     }
+
+    // Listen for incoming connections (allowing a maximum of 3 sockets at the same time)
     if (listen(server_fd, 3) < 0) {
         perror("listen");
+        printlog("Server: Listen failed");
         exit(EXIT_FAILURE);
     }
 
+    // Main server loop
     while (1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
             perror("accept");
+            printlog("Accept failed");
+            continue;
         }
-        exit(EXIT_FAILURE);
-        read(new_socket, buffer, 1024);
-        for (int i = 0; buffer[i]; i++) {
-            buffer[i] = toupper(buffer[i]);
+
+        if (pthread_create(&thread_id, NULL, handle_client, (void*)&client_socket) != 0) {
+            perror("pthread_create");
+            printlog("Pthread_create failed");
+        } else {
+            pthread_detach(thread_id);  // detach the thread to handle its own resources
         }
-        send(new_socket, buffer, strlen(buffer), 0);
-        close(new_socket);
     }
 
-    printf("end");
+    // Close the server socket
+    close(server_fd);
+    printlog("Server: End");
     return 0;
 }
